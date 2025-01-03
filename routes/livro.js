cabda936486be0741c9c178a8b09473f2af1d669
ardszsantos <lokgamer01@gmail.com>
2025-01-03 13:27:23 -0300

    added delete and patch

--- routes/livro.js
+++ routes/livro.js
@@ -9,5 +9,11 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     res.send("Você fez uma requisição do tipo POST")
 })
+router.patch('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo PATCH")
+})
+router.delte('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo PUT")
+})
 
 module.exports = router
