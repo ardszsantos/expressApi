fec1fdab7e0f43234b86f52b7c43b87bdeaa1e52
ardszsantos <lokgamer01@gmail.com>
2025-01-03 13:26:18 -0300

    feat: adding a post route

--- routes/livro.js
+++ routes/livro.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.send("Olá mundo")
 });
 
+router.post('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo POST")
+})
+
 module.exports = router
