940b83197759a9dd2783e4854ee9454e4747eac7
ardszsantos <lokgamer01@gmail.com>
2025-01-04 13:42:22 -0300

    changed the router way of dealing with routes

--- routes/livro.js
+++ routes/livro.js
@@ -1,23 +1,12 @@
 const { Router } = require("express");
+const { getLivros, postLivros, patchLivros, deleteLivros } = require("../controladores/livro")
 const router = Router()
 
 
-router.get('/', (req, res) => {
-    try {
-        res.send("Olá mundo")
-    } catch (error) {
-        res.status(500)
-    }
-});
+router.get('/', getLivros)
+router.post('/', postLivros)
+router.patch('/', patchLivros)
+router.delete('/', deleteLivros)
 
-router.post('/', (req, res) => {
-    res.send("Você fez uma requisição do tipo POST")
-})
-router.patch('/', (req, res) => {
-    res.send("Você fez uma requisição do tipo PATCH")
-})
-router.delete('/', (req, res) => {
-    res.send("Você fez uma requisição do tipo PUT")
-})
 
 module.exports = router
