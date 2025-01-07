cc591468e8ed72d6428cff88d6f9801939e872e9
ardszsantos <lokgamer01@gmail.com>
2025-01-07 10:15:17 -0300

    using get livro

--- routes/livro.js
+++ routes/livro.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
-const { getLivros, postLivros, patchLivros, deleteLivros } = require("../controladores/livro")
+const { getLivros, postLivros, patchLivros, deleteLivros, getLivro } = require("../controllers/livro")
 const router = Router()
 
 
 router.get('/', getLivros)
+router.get('/:id', getLivro)
+
 router.post('/', postLivros)
 router.patch('/', patchLivros)
 router.delete('/', deleteLivros)
