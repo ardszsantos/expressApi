245f09b31d4e5025c6a3f9e1bcd1779622a6793d
ardszsantos <lokgamer01@gmail.com>
2025-01-09 11:26:28 -0300

    :id on patch route

--- routes/livro.js
+++ routes/livro.js
@@ -1,13 +1,13 @@
 const { Router } = require("express");
-const { getLivros, postLivros, patchLivros, deleteLivros, getLivro } = require("../controllers/livro")
+const { getLivros, postLivro , patchLivros, deleteLivros, getLivro } = require("../controllers/livro")
 const router = Router()
 
 
 router.get('/', getLivros)
 router.get('/:id', getLivro)
 
-router.post('/', postLivros)
-router.patch('/', patchLivros)
+router.post('/', postLivro)
+router.patch('/:id', patchLivros)
 router.delete('/', deleteLivros)
 
 
